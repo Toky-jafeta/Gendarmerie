c614e5a652ffa754fbac27039d42e76984a5785f
ainasoa <arabenjanotahina@bocasay.com>
2024-01-22 21:20:23 +0300

    commit divers/pv

--- gendarmerie/gendarmerie/settings.py
+++ gendarmerie/gendarmerie/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'auteurs',
     'victimes',
-    'problemes_sociaux'
+    'problemes_sociaux',
+    'divers',
+    'proces_verbaux'
 ]
 
 MIDDLEWARE = [
---
+++ gendarmerie/proces_verbaux/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProcesVerbauxConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'proces_verbaux'
